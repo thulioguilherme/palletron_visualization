0069fbc9d22592c2f51d71b6e195a9dbceea81a2
thulioguilherme <thulioguilherme@hotmail.com>
2023-11-09 09:45:49 -0300

    formatted code style

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,6 +1,6 @@
-from launch import LaunchDescription, LaunchContext
+from launch import LaunchDescription
 
-from launch.actions import OpaqueFunction, DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
@@ -9,28 +9,6 @@ from launch_ros.substitutions import FindPackageShare
 
 from nav2_common.launch import ReplaceString
 
-def launch_setup(context: LaunchContext):
-    # Arguments
-    namespace = LaunchConfiguration('namespace')
-    config_file = LaunchConfiguration('config_file')
-
-    # Get full path to the Rviz configuration file
-    config_file_path = PathJoinSubstitution([FindPackageShare('palletron_visualization'), 'rviz', config_file])
-
-    # Replace robot namespace in Rviz file
-    namespaced_config_file = ReplaceString(
-        source_file = config_file_path,
-        replacements = {'<robot_namespace>': ('/', namespace)})
-
-    # Declare nodes
-    rviz2_node = Node(
-        package = 'rviz2',
-        executable = 'rviz2',
-        namespace = namespace,
-        arguments = ['-d', namespaced_config_file]
-    )
-
-    return [rviz2_node]
 
 def generate_launch_description():
     declared_arguments = []
@@ -39,17 +17,34 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'namespace',
-            default_value = 'palletron1',
-            description = 'Top-level namespace.'
-        )
-    )
+            default_value='palletron1',
+            description='Top-level namespace.'))
 
     declared_arguments.append(
         DeclareLaunchArgument(
             'config_file',
-            default_value = 'minimal_robot_view.rviz',
-            description = 'Name of the Rviz configuration file to use.'
-        )
-    )
+            default_value='minimal_robot_view.rviz',
+            description='Name of the Rviz configuration file to use.'))
 
-    return LaunchDescription(declared_arguments + [OpaqueFunction(function = launch_setup)])
+    # Initialize arguments
+    namespace = LaunchConfiguration('namespace')
+    config_file = LaunchConfiguration('config_file')
+
+    # Get full path to the Rviz configuration file
+    config_file_path = PathJoinSubstitution([FindPackageShare('palletron_visualization'),
+                                             'rviz',
+                                             config_file])
+
+    # Replace robot namespace in Rviz file
+    namespaced_config_file = ReplaceString(
+        source_file=config_file_path,
+        replacements={'<robot_namespace>': ('/', namespace)})
+
+    # Declare nodes
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        namespace=namespace,
+        arguments=['-d', namespaced_config_file])
+
+    return LaunchDescription(declared_arguments + [rviz2_node])
